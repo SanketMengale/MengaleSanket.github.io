959caca382a050cf3e1b3f7498ea683e95710f52
SanketMengale <sanketmengale55@gmail.com>
2026-04-05 21:48:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const sections = document.querySelectorAll('.section');
+
+window.addEventListener('scroll', () => {
+    sections.forEach(sec => {
+        const top = window.scrollY;
+        const offset = sec.offsetTop - 300;
+
+        if (top > offset) {
+            sec.classList.add('show');
+        }
+    });
+});
